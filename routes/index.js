764ca0b121700fb73db716cba44b20ce28502115
Panav <apple@apples-MacBook-Pro.local>
2022-07-10 16:44:33 +0530

    Add route to app and add some routes and their controllers

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,12 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home_controller')
 router.get('/',homeController.home)
+// if url has users in it then pass on the value of router value in users router file to app
+router.use('/users',require('./users'))
+
+// for any further routes, access like
+// router.use('/routername',require('./route/routerfile'))
+
+router.use('/posts',require('./posts'))
 
 module.exports=router
