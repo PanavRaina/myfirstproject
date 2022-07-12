fced8c53042f7285eb79a76f40b9bb396657a2c7
Panav <apple@apples-MacBook-Pro.local>
2022-07-12 12:46:32 +0530

    add more views

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const port = 8000
 const app = express()
+const expressLayouts=require('express-ejs-layouts')
 
-// telling this file to use router -> index.js for routing
+app.use(express.static('./assets'))
+app.use(expressLayouts)
+
+
+// telling this file to use router(folder) -> index.js for routing
 app.use('/',require('./routes/index'))
 app.set('view engine','ejs')
 
 app.set('views','./views')
+
+// placing style tag in head section when individual pages are getting wrapped around layout
+app.set('layout extractStyles',true)
+
+// placing script tag in correct position when individual pages are getting wrapped around layout
+app.set('layout extractScripts',true)
+
 
 app.listen(port,function(error){
     if(error)
